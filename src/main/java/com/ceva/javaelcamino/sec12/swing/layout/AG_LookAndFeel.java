34510f7da7d3357cdd2445257bdad690cee0f36e
cevadev <carlos.villanueva.altuna@gmail.com>
2022-12-14 22:47:40 -0300

    Java curso El Camino II - Java 2D

---
+++ src/main/java/com/ceva/javaelcamino/sec12/swing/layout/AG_LookAndFeel.java
@@ -0,0 +1,61 @@
+  /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ceva.javaelcamino.sec12.swing.layout;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+/**
+ * GridBagLayout
+ */
+public class AG_LookAndFeel {
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        }
+        catch(Exception ex){
+            System.out.println("Look and Feel error");
+        }
+        
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Look & Feel demo");
+                GridBagLayout layout = new GridBagLayout();
+                // asignamos el nuevo layout
+                frame.getContentPane().setLayout(layout);
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame.setMinimumSize(new Dimension(400, 300));
+                // centramos la ventana
+                frame.setLocationRelativeTo(null);
+                
+                for(int n = 0; n < 8; n++){
+                    JButton button = new JButton("Test " + n);
+                    GridBagConstraints c = new GridBagConstraints();
+                    c.gridx = n % 3;
+                    c.gridy = n / 3;
+                    c.insets = new Insets(4,4,4,4);
+                    
+                    frame.add(button, c);
+                }
+                frame.setVisible(true);
+            }
+        });
+    }
+}
