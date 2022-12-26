5a6d721e3286fd883dda40a47e53be4ecef530ac
cevadev <carlos.villanueva.altuna@gmail.com>
2022-12-26 18:33:35 -0300

    Course Java el Camino II - Drawing text formatted with Awt

--- src/main/java/com/ceva/javaelcamino/sec13/java2D/AG_TextFormatterAWT.java
+++ src/main/java/com/ceva/javaelcamino/sec13/java2D/AG_TextFormatterAWT.java
@@ -44,7 +44,7 @@ public class AG_TextFormatterAWT extends JPanel {
         }
     }
     
-    private void drawText(Graphics2D g2d){
+    private void drawText0(Graphics2D g2d){
         // 1. Establecemos el FontMetrics
         FontMetrics fm = g2d.getFontMetrics();
         // 2. definimos un marge izquierdo y derecho
@@ -56,22 +56,111 @@ public class AG_TextFormatterAWT extends JPanel {
         // 5 comenzando por el margin, dibujamos en la coordena xPos
         int xPos = margin;
         // 6 permite ubicarnos en la parte superior para dibujar
-        int yPost = fm.getAscent();
+        int yPos = fm.getAscent();
         for(String word : words){
             // determinamos cual va a ser el ancho de la sgte palabra
             int width = fm.stringWidth(word);
-            g2d.drawString(word, xPos, yPost);
+            // validamos si se supera la linea, escribir la palabra en la linea siguiente
+            if((xPos+width)> lineWidth){
+                // pasamos a la sgte linea
+                xPos = margin;
+                yPos += fm.getHeight();
+                // dibujamos mientras mientras haya espacio para el renglon completo
+                /*if((yPos+fm.getDescent()) >= getHeight()){
+                    break;
+                }*/
+                // el en caso que deseemos dibujar una linea parcialmente
+                // a la linea base (yPos) restamos el ascent
+                if((yPos - fm.getAscent()) >= getHeight()){
+                    break;
+                }
+            }
+            
+            g2d.drawString(word, xPos, yPos);
             // actualizamos la posicion x, con el espacio despues de la palabra
-            // xPos = espacio en blanco + espacio de la palabra
-            xPos = spaceWidth + width;
+            // xPos = espacio en blanco + tamaño de la palabra
+            xPos += spaceWidth + width;
         }
+    }
+    
+    private void drawText(Graphics2D g2d){
+        FontMetrics fm = g2d.getFontMetrics();
+        
+        // numero de columna actual
+        int curColumn = 0;
+        int margin = 10;
+        // ancho de la columna = (tamanio de ventana - el mananio del margen * (numero de columnas +1))/ numColumns
+        int lineWidth = (getWidth() - margin * (numColumns+1)) / numColumns;
+        int spaceWidth = fm.charWidth(' ');
+        int xPos = margin;
+        int yPos = fm.getAscent();
+        // acumulado del ancho de una linea
+        int accWidth = 0;
+        
+        // recorremos las palabras a dibujar
+        for(String word : words){
+            // calculamos el ancho de la palabra
+            int width = fm.stringWidth(word);
+            // validamos si el acumulado para el tamanio de la columna actual+tamanio de la nueva palabra es mayor
+            // al tamanio maximo de la columna
+            if((accWidth+width) > lineWidth){
+                // cambiamos al sgte renglon
+                yPos += fm.getHeight();
+                // si el nuevo renglon supera el maximo
+                if((yPos + fm.getDescent()) >= getHeight()){
+                    // reset a la posicion y la colocamos al inicio de la sgte columna
+                    yPos = fm.getAscent();
+                    // pasamos a la sgte columna
+                    curColumn++;
+                    // dejamos de dibujar cuando superamos el numero de columnas
+                    if(curColumn >= numColumns){
+                        break;
+                    }
+                }
+                // reset el acumulado ancho de columna
+                accWidth = 0;
+                // formula donde determina la posicion dond iniciamos a dibujar
+                xPos = margin + (lineWidth+margin)*curColumn;
+            }
+            // dibujamos la palabra
+            g2d.drawString(word, xPos, yPos);
+            // actualizamos el acumulado para el tamanio de la columna actual
+            accWidth += spaceWidth + width;
+            // actualizamos xPost con el valor de la palabra (width) + un espacio (spaceWidth)
+            xPos += spaceWidth + width;
+        }   
+    }
+    
+    private void drawTextJustified(Graphics2D g2d){
+        FontMetrics fm = g2d.getFontMetrics();
+        // para dibujar justificado debo medir toda la columna y hasta encontrar las palabras que
+        // va a ocupar la columna, calculamos el tamanio de cada espacio para que quede jstificado
+        List<String> curLine = new LinkedList<>();
+        // columna inicial
+        int curColumn = 0;
+        // margen por defecto
+        int margin = 10;
+        // ancho de cada columna
+        int lineWidth = (getWidth() - margin * (numColumns+1)) / numColumns;
+        // tamanio de un espacio
+        int spaceWidth = fm.charWidth(' ');
+        int xPos = margin;
+        int yPos = fm.getAscent();
+        // controlamos el espacio ya ocupado para la columna actual
+        int accWidth = 0;
+        // controlamos el ancho que ocupan las palabras sin tomar en cuenta los espacios
+        int curLineWidth = 0;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D)g;
-        drawText(g2d);
+        if(justify){
+            drawTextJustified(g2d);
+        }else{
+            drawText(g2d);
+        }
     }
     
     
@@ -102,7 +191,7 @@ public class AG_TextFormatterAWT extends JPanel {
             public void run() {
                 JFrame frame = new JFrame("TextFormatterAWT");
                 frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                frame.setMinimumSize(new Dimension(400,400));
+                frame.setMinimumSize(new Dimension(400, 400));
                 frame.setLocationRelativeTo(null);
                 frame.setContentPane(new AG_TextFormatterAWT(str, columns, true));
                 frame.setVisible(true);
