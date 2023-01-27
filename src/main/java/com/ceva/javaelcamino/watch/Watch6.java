db91f00048d1861148ddeca06f40eeb33f72d1fe
cevadev <carlos.villanueva.altuna@gmail.com>
2023-01-26 22:39:56 -0300

    Curso Java el camino II - Java Watch

--- src/main/java/com/ceva/javaelcamino/watch/Watch6.java
+++ src/main/java/com/ceva/javaelcamino/watch/Watch6.java
@@ -28,6 +28,7 @@ import javax.swing.SwingUtilities;
  * @author PC
  */
 public class Watch6 extends JPanel {
+
     private final Color backgroundColor = new Color(0x404c0); // color de fondo
     private final Color hourColor = Color.BLACK; // color manecilla de la hora
     private final Color minuteColor = Color.BLACK; // color manecilla minuto
@@ -36,65 +37,94 @@ public class Watch6 extends JPanel {
     private final Color labelColor = Color.BLACK; // color del marco del reloj
     private final Color gradientA = new Color(224, 224, 224, 255);
     private final Color gradientB = new Color(32, 32, 32, 144);
-    
+    private final Color shadowColor = new Color(0x0, 0x0, 0x0, 0x30);
+    private final String title = "Círculo de 5tas";
+
     private Font fSmall;
     private Font fMedium;
     private Font fLarge;
-    
+    private Font fTitleSmall;
+    private Font fTitleMedium;
+    private Font fTitleLarge;
+
     private int hour;
     private int minute;
     private int second;
-    
+
     private int hourStroke = 12;
     private int minuteStroke = 5;
     private int secondStroke = 3;
-    
+
     private javax.swing.Timer timer;
-    
+
     // implementando el timer
-    public Watch6(){
+    public Watch6() {
         super();
         // actualizamos la hora
         updateTime();
         // set up el timer cada segundo
-        timer = new javax.swing.Timer(1000, (e)->{
+        timer = new javax.swing.Timer(1000, (e) -> {
             // actulizamos la hora y redibujamos el reloj
             updateTime();
             repaint();
         });
         timer.start();
     }
-    
-    private void stopTimer(){
+
+    private void stopTimer() {
         timer.stop();
     }
-    
-    private Font getSmallFont(Graphics2D g2d){
-        if(fSmall != null){
+
+    private Font getSmallFont(Graphics2D g2d) {
+        if (fSmall != null) {
             return fSmall;
         }
         fSmall = g2d.getFont().deriveFont(24f);
         return fSmall;
     }
     
-    private Font getMediumFont(Graphics2D g2d){
-        if(fMedium != null){
+    private Font getSmallTitleFont(Graphics2D g2d) {
+        if (fTitleSmall != null) {
+            return fTitleSmall;
+        }
+        fTitleSmall = g2d.getFont().deriveFont(12f);
+        return fTitleSmall;
+    }
+
+    private Font getMediumFont(Graphics2D g2d) {
+        if (fMedium != null) {
             return fMedium;
         }
         fMedium = g2d.getFont().deriveFont(36f);
         return fMedium;
     }
     
-    private Font getLargeFont(Graphics2D g2d){
-        if(fLarge != null){
+    private Font getMediumTitleFont(Graphics2D g2d) {
+        if (fTitleMedium != null) {
+            return fTitleMedium;
+        }
+        fTitleMedium = g2d.getFont().deriveFont(18f);
+        return fTitleMedium;
+    }
+
+    private Font getLargeFont(Graphics2D g2d) {
+        if (fLarge != null) {
             return fLarge;
         }
         fLarge = g2d.getFont().deriveFont(72f);
         return fLarge;
     }
     
+    private Font getLargeTitleFont(Graphics2D g2d) {
+        if (fTitleLarge != null) {
+            return fTitleLarge;
+        }
+        fTitleLarge = g2d.getFont().deriveFont(36f);
+        return fTitleLarge;
+    }
+
     // metodo para saber/obtener la hora
-    private void updateTime(){
+    private void updateTime() {
         // obtenemos instancia de Calendar
         Calendar cal = Calendar.getInstance();
         // retornamos hora minto segundo
@@ -102,7 +132,7 @@ public class Watch6 extends JPanel {
         minute = cal.get(Calendar.MINUTE);
         second = cal.get(Calendar.SECOND);
     }
-    
+
     // Tamanio de letra (chico, mediano , grande)
     @Override
     protected void paintComponent(Graphics g) {
@@ -127,7 +157,7 @@ public class Watch6 extends JPanel {
         Rectangle frame = new Rectangle(bounds.width / 2 - size / 2, bounds.height / 2 - size / 2, size, size);
         g2d.setColor(faceColor); // color de linea
         g2d.fillOval(frame.x, frame.y, frame.width, frame.height); // dibujamos el circulo en le medio
-        
+
         Stroke simpleStroke = g2d.getStroke();
         Stroke boldStroke = new BasicStroke(2);
         g2d.setColor(Color.BLACK);
@@ -139,20 +169,20 @@ public class Watch6 extends JPanel {
         int centerY = frame.y + frame.height / 2;
         double radius = frame.width / 2 - 10; // radio del circulo, con un marge de 5
         double innerRadius = radius * 0.9; // definimos un radio interno que es igual al 90% del radio
-        
+
         // gradiente
         Paint savePaint = g2d.getPaint();
         RadialGradientPaint rp = new RadialGradientPaint(
                 // definimos un punto coordenada x que se encuentra en e centro del relos
-                (float)(centerX - Math.cos(Math.toRadians(45)) * radius),
+                (float) (centerX - Math.cos(Math.toRadians(45)) * radius),
                 // definimos un punto coordenada y que se encuentr en el centro del reloj
-                (float)(centerY - Math.sin(Math.toRadians(45)) * radius),
+                (float) (centerY - Math.sin(Math.toRadians(45)) * radius),
                 // 
-                (float)radius * 3.5f,
-                new float[] {0.0f, 0.5f},
-                new Color[] {gradientA, gradientB});
+                (float) radius * 3.5f,
+                new float[]{0.0f, 0.5f},
+                new Color[]{gradientA, gradientB});
         // end gradiente
-        
+
         g2d.setPaint(rp);
         // pintamos utilizando el gradiente definido
         g2d.fillOval(frame.x + 4, frame.y + 4, frame.width - 8, frame.height - 8);
@@ -161,31 +191,40 @@ public class Watch6 extends JPanel {
         g2d.setColor(labelColor);
         // dibujamos un marco grueso para el reloj
         g2d.setStroke(new BasicStroke(10));
-        g2d.drawOval(frame.x+4, frame.y+4, frame.width-8, frame.height-8);
+        g2d.drawOval(frame.x + 4, frame.y + 4, frame.width - 8, frame.height - 8);
         g2d.setStroke(simpleStroke);
 
         // Letra de tamanio 36
 //        g2d.setFont(g2d.getFont().deriveFont(36f));
-
         // establecemos el tamanio del font, de acuerdo al tamanio en pixeles del frame
         Font f;
-        if(frame.width < 400){
+        Font fTitle;
+        if (frame.width < 400) {
             f = getSmallFont(g2d);
-        }else if(frame.width < 800){
+            fTitle = getSmallTitleFont(g2d);
+        } else if (frame.width < 800) {
             f = getMediumFont(g2d);
-        }else{
+            fTitle = getMediumTitleFont(g2d);
+        } else {
             f = getLargeFont(g2d);
+            fTitle = getLargeTitleFont(g2d);
         }
         g2d.setFont(f);
         FontMetrics fm = g2d.getFontMetrics();
 
-        
         double angle;
+        // coordenadas para hora, minuto, segundo
+        int coordXHora, coordXMin, coordXSeg;
+        int coordYHora, coordYMin, coordYSec;
+
         for (int n = 0; n < 60; n++) {
             // angulo que tiene cada segundo 360/60
             angle = (270 + n * (360 / 60)) % 360.0; // mod 360 nos permite un rango entre 0 y 360
             double coseno = Math.cos(Math.toRadians(angle));// calculamos el coseno de angulo pero primero lo convertimos a radianes
             double seno = Math.sin(Math.toRadians(angle));// calculamos el seno del a angulo
+            coordXHora = (int) (coseno * radius);
+            coordYHora = (int) (seno * radius);
+
             // si n es divisible por 5 significa que estoy en la marca de una division de hora
             if ((n % 5) == 0) {
                 // dibujamos la lineas de hora
@@ -219,30 +258,66 @@ public class Watch6 extends JPanel {
 
             // dibujamos la linea
             g2d.drawLine((int) (centerX + coseno * innerRadius), (int) (centerY + seno * innerRadius),
-                    (int) (centerX + coseno * radius), (int) (centerY + seno * radius));
+                    (int) (centerX + coordXHora), (int) (centerY + coordYHora));
         }
+        // dibujamos la marca del reloj
+        g2d.setFont(fTitle);
+        Rectangle2D r = g2d.getFontMetrics().getStringBounds(title, g2d);
+        // centrado a lo horizontal pero en lo vertical a una distancia de 30% del radio
+        g2d.drawString(title, (int)(centerX - r.getWidth()/2), (int)(centerY + radius * 0.3));
+        
         // lineas para las manecillas
         double hourRadius = radius * 0.5;
         double minRadius = radius * 0.7;
         double secRadius = radius * 0.9;
+        
+        coordXHora = (int)(Math.cos(Math.toRadians((270+(double)hour*(360/12)+(double)minute*360/(12*60)))%360) * hourRadius);
+        coordYHora = (int)(Math.sin(Math.toRadians((270+(double)hour*(360/12)+(double)minute*360/(12*60)))%360) * hourRadius);
+        
+        coordXMin = (int)(Math.cos(Math.toRadians((270 + (360 / 60) * (double) minute) % 360.0)) * minRadius);
+        coordYMin = (int)(Math.sin(Math.toRadians((270 + (360 / 60) * (double) minute) % 360.0)) * minRadius);
+        
+        coordXSeg = (int) (Math.cos(Math.toRadians((270 + (360 / 60) * (double) second) % 360.0)) * secRadius);
+        coordYSec = (int) (Math.sin(Math.toRadians((270 + (360 / 60) * (double) second) % 360.0)) * secRadius);
+        
+        // sobras para efecto 3D de las manecillas segundos
+        g2d.setStroke(new BasicStroke(secondStroke));
+        g2d.setColor(shadowColor);
+        // dubujamos la sombra para el segundero y le agregamos 3 pixels
+        g2d.drawLine((int)(centerX - coordXSeg*0.2)+3, (int)(centerY - coordYSec*0.2)+3, centerX + coordXSeg+3, centerY + coordYSec+3);
+        
+        // sombras para efecto 3D de las manecillas minutos
+        g2d.setStroke(new BasicStroke(minuteStroke));
+        g2d.drawLine((int)(centerX - coordXMin*0.15)+2, (int)(centerY - coordYMin*0.2)+2, centerX + coordXMin+2, centerY + coordYMin+2);
+        
+        // sombras para efecto 3D de las manecillas horas
+        g2d.setStroke(new BasicStroke(hourStroke));
+        g2d.drawLine(centerX+2, centerY+2, centerX + coordXHora+2, centerY + coordYHora+2);
+        
         // manecilla de horas
         g2d.setStroke(new BasicStroke(hourStroke));
         g2d.setColor(hourColor);
-        angle = (270 + hour * (360.0 / 12) + minute * (360.0 / 12 / 60)) % 360.0;
-        g2d.drawLine(centerX, centerY, (int)(centerX + Math.cos(Math.toRadians(angle)) * hourRadius), 
-                                       (int)(centerY + Math.sin(Math.toRadians(angle)) * hourRadius));
+//        angle = (270 + hour * (360.0 / 12) + minute * (360.0 / 12 / 60)) % 360.0;
+        // circulo en el centro de las manecillas
+        g2d.fillOval(centerX-10, centerY-10, 20, 20);
+        g2d.drawLine(centerX, centerY, centerX + coordXHora, centerY + coordYHora);
+        
         // manecilla de minutos
-        angle = (270 + minute * (360 / 60)) % 360.0;
+//        angle = (270 + minute * (360 / 60)) % 360.0;
         g2d.setColor(minuteColor);
         g2d.setStroke(new BasicStroke(minuteStroke));
-        g2d.drawLine(centerX, centerY, (int)(centerX + Math.cos(Math.toRadians(angle)) * minRadius), 
-                                       (int)(centerY + Math.sin(Math.toRadians(angle)) * minRadius));
+        g2d.drawLine((int)(centerX - coordXMin*0.15), (int)(centerY - coordYMin*0.2), centerX + coordXMin, centerY + coordYMin);
+        
         // manecilla de segundos
-        angle = (270 + second * (360 / 60)) % 360.0;
+//        angle = (270 + second * (360 / 60)) % 360.0;
         g2d.setColor(secondColor);
         g2d.setStroke(new BasicStroke(secondStroke));
-        g2d.drawLine(centerX, centerY, (int)(centerX + Math.cos(Math.toRadians(angle)) * secRadius), 
-                                       (int)(centerY + Math.sin(Math.toRadians(angle)) * secRadius));
+        // dibujamos el segundero antes del centro
+        g2d.drawLine((int)(centerX - coordXSeg*0.2), (int)(centerY - coordYSec*0.2), centerX + coordXSeg, centerY + coordYSec);
+        
+        // pequenio circulo en el medio de las manecillas
+        g2d.setColor(backgroundColor);
+        g2d.fillOval(centerX-3, centerY-3, 6, 6);
     }
 
     public static void main(String[] args) {
@@ -252,15 +327,15 @@ public class Watch6 extends JPanel {
             frame.setTitle("Watch 6");
             frame.setMinimumSize(new Dimension(400, 400));
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            
+
             // cuando se cierra la ventana, terminamos el timer
-            frame.addWindowListener(new WindowAdapter(){
+            frame.addWindowListener(new WindowAdapter() {
                 @Override
                 public void windowClosed(WindowEvent e) {
                     mainPanel.stopTimer();
                 }
             });
-            
+
             frame.setContentPane(mainPanel);
             frame.pack();
             frame.setLocationRelativeTo(null);
