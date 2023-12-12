803aa9f95802744017e6eeb5da43d92a9a018f2d
cevadev <carlos.villanueva.altuna@gmail.com>
2023-12-12 10:59:53 -0300

    Curso Java El Camino II - Condicionales

---
+++ src/com/ceva/section1/Condicionales.java
@@ -0,0 +1,48 @@
+package com.ceva.section1;
+
+public class Condicionales {
+    private static void calificar(int nota){
+        if(nota < 0 || nota > 20){
+            System.out.println("La nota no puede ser menos a 0 o mayor a 20");
+        }else if(nota <= 5){
+            System.out.println("Su calificion es D");
+        }else if(nota <= 10){
+            System.out.println("Su calificacion es C");
+        }else if(nota <= 15){
+            System.out.println("Su calificacion es B");
+        }else if(nota <= 18){
+            System.out.println("Su calificacion es A");
+        }else{
+            System.out.println("Su calificacion es A+.. Excelente!");
+        }
+    }
+
+    // 9 2 8 1
+    private static void numeroMayor(int a, int b, int c, int d){
+        if (a > b) {
+            if(a > c){
+                if(a > d){
+                    System.out.println("El numero mayor es " + a);
+                }else{
+                    System.out.println("El numero mayor es " + d);
+                }
+            }else if(c > d){
+                System.out.println("El numero mayor es " + c);
+            }else{
+                System.out.println("El numero mayor es " + d);
+            }
+        }else if(b > c){
+            if(b > d){
+                System.out.println("El numero mayor es " + b);
+            }else{
+                System.out.println("El numero mayor es " + d);
+            }
+        }else{
+            System.out.println("El numero mayor es " + c);
+        }
+    }
+    public static void main(String[] args) {
+        numeroMayor(2569,354635,1587,3658);
+        //calificar(7);
+    }
+}
