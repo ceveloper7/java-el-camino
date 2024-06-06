469341acfaae95a77d2ed62c43320e0f6ad8990d
Carlos V <cevabarcvilla@gmail.com>
2024-06-05 23:17:08 -0300

    Curso Java EL Camino II - Section 7 Java Api 2

--- src/com/ceva/section7/apijava2/HCollections.java
+++ src/com/ceva/section7/apijava2/HCollections.java
@@ -77,7 +77,21 @@ public class HCollections {
         p2.age = 21;
     }
 
+    /**
+     * EL objeto Iterator permite recorrer los objetos de una coleccion independiente de como internamente la coleccion esta implementada.
+     * Un iterator recorrer todos los elementos de la coleccion pero lo hara de la manera mas conveniente y no garantiza se que se recorran
+     * en el orden que fueron insertados.
+     * .iterator() -> retorna un ojbeto iterator
+     * .hasNext() -> retorna true si aun existen elementos por recorrer en la coleccion
+     * .next() -> retorna el siguiente elemento en la coleccion
+     */
     private static void iteratorTest() {
+        /**
+         * Implementacion de Set
+         * HashSet -> guarda sus elementos utilizando una tabla basada en el hashcode de cada objeto. Esta implementacion es eficiente para encontrar
+         *            elementos y muy eficiente para encontrarlos aleatoriamente. Su desventaja es que cuando su tabla interna se llena es
+         *            necesario expandirla y volver a acomadar todos sus elementos
+         */
         Set set = new HashSet();
         set.add("uno");
         set.add("dos");
@@ -93,6 +107,11 @@ public class HCollections {
     }
 
     private static void linkedHashSetTest() {
+        /**
+         * Implementacion de Set
+         * LinkedHashSet -> Parecida a HashSet pero ademas de guardar sus elementos en una tabla hash tambien los guarda en una lista enlazada.
+         *                  por lo que puede recorrerlos en el mismo orden que fueron agregados a la coleccion
+         */
         Set set = new LinkedHashSet();
         set.add("uno");
         set.add("dos");
@@ -102,13 +121,20 @@ public class HCollections {
         set.add("seis");
 
         for (Iterator it=set.iterator(); it.hasNext(); )  {
-            String s = (String) it.next();
+            String s = (String) it.next(); // retornamos los elementos en el mismo orden que fueron agregados
             System.out.println(s);
         }
     }
 
     private static void treeSetTest() {
+        /**
+         * Implementacion de Set
+         * TreeSet -> esta implementacion guarda la info deacuerdo a su orden natural o de acuerdo a un comparador de tal forma que la info se
+         *            retorna de manera ordenada. Se puede usar para odenar elementos rapidamente. Agregamos elementos y automaticamente
+         *            los elementos seran ordenados.
+         */
         Set set = new TreeSet();
+        // al tratarse de string el orden natural es el ordenamiento alfabetico por lo que cuatro y cinco estaran al principio de la lista
         set.add("uno");
         set.add("dos");
         set.add("tres");
@@ -122,7 +148,20 @@ public class HCollections {
         }
     }
 
+    /**
+     * Implementacion de List: una lista es una coleccion de elementos que pueden estar repetidos. A diferencia de Set las listas pueden encontrar
+     *                         elemento en un posicion especifica comenzando con 0. EL orden de numeracion de una lista es el mismo en que los
+     *                         elementos fueron insertados.
+     */
     private static void listTest() {
+        /**
+         * Implementacion List:
+         * ArrayList -> es una lista respaldada por un arreglo de objetos, es decir, es un array. LOs array son buenos para encontar elementos
+         *              aleatoriamente porque solo hay que consultar el indice correspondiente. La desventaja es que el tamano de un array es fijo
+         *              y al llenarse, java debe crear un nuevo array mover todos los elementos para hacer mas espacio. A borrar un elementos java
+         *              debera comodar el resto de la coleccion por lo que un arraylist no es la mejor opcion si se estan insertando y borrando
+         *              datos constantemente
+         */
         List list = new ArrayList();
         list.add("uno");
         list.add("dos");
@@ -141,7 +180,43 @@ public class HCollections {
         }
     }
 
+    /**
+     * Implementacion List
+     * LinkedList -> esta es una lista respaldada por nodos doblemente enlazados. un nodo doblemente enlazado es objeto que contiene el valor del
+     *               elemento y una referencia al elementos posterior y anterior.
+     * Ejemplo: de una lista doblemente enlazada
+     * Nodo1 esta conectado con el Nodo2
+     * Nodo2 esta conectado con el Nodo1
+     * Nodo2 esta conectado con el Nodo3
+     * Nodo3 esta conectado con el Nodo2
+     *
+     *
+     * Recorrer y hacer crecer una lista usando LinkedList es facil ya que lo necesario es crear un nuevo nodo cuando tengo un nuevo elemento. SIn
+     * embargo, si queremos acceder a los elementos por indice ya no es tan facil. Por ejemplo si queremos acceder al nodo 2 linkedList empieza
+     * por la posicion 0 hasta llegar al elemento solicitado.
+     * Es mas eficiente borrar elementos de un linkedlist ya que para borrar un elemento es suficiente quitar las referencias o enlaces del nodo
+     * en cuestion.
+     * Conclusion: LinkedList es eficiente para agregar y para borrar elementos constantemente pero no es eficiente para acceder a un elemento
+     * aleatoramente es decir por un posicion especifica.
+     */
+
+    /**
+     * Implementacion List
+     * Stack -> esta implementacion recibe este nombre porque se comporta como una pila de platos que utiliza el metodo LastIn-FirstOut o LIFO o
+     *          decimos que el ultimo en entrar es el primero en salir.
+     */
+
+
+    /**
+     * Diccionarios: un diccionario es una coleccion de elementos respaldados por un indice. los diccionarios son empleados para almacenar info
+     *               que se necesatia consultar rapidamente. AL igual que set los elementos de un map son unicos
+     */
     private static void hashMapTest() {
+        /**
+         * Implementacion Map: LOs elementos de un Map son unicos si se agrega dos objetos con el mismo key (Integer) el elemento sera reemplazado.
+         * HashMap -> es un diccionario indexado con codigos hash para consultas rapidas pero debido a esto un iterator no retorna los
+         *            elementos en el orden que fueron insertados
+         */
         Map map = new HashMap();
         map.put(1, "uno");
         map.put(2, "dos");
@@ -149,7 +224,9 @@ public class HCollections {
         map.put(4, "cuatro");
         map.put(5, "cinco");
 
+        // recorremos elementos impares
         for (int n=1; n<=5; n+=2)
+            // .get() aplica un autoboxing y convierte int n a Integer n
             System.out.println(map.get(n));
 
         System.out.println();
@@ -158,21 +235,35 @@ public class HCollections {
             System.out.println(it.next());
         }
 
-        System.out.println();
-        // Recorrer pares indice-valor del diccionario
+        System.out.println("Recorremos indice-valor del Map");
+        // Recorrer pares indice-valor del diccionario. .entrySet() retorna un Set donde cada elemento contiene tanto el indice como el valor
         for (Iterator it=map.entrySet().iterator(); it.hasNext(); ) {
             Map.Entry entry = (Map.Entry) it.next();
+            // este metodo es mas eficiente ya que con Entry podemos acceder al key y al value
             System.out.println("key: " + entry.getKey() + ", value=" + entry.getValue());
         }
 
-        System.out.println();
-        // Recorrer indices del diccionario
+        System.out.println("Recorremos solo valores del Map");
+        // Recorrer indices del diccionario. .keySet() retorna un Set que apunta a todos los indices del Map
         for (Iterator it=map.keySet().iterator(); it.hasNext(); ) {
+            // .it.next() retorna el indice y map.get() retorna el valor del indice
             System.out.println(map.get(it.next()));
         }
     }
 
+    /**
+     * Implementacion de Map
+     * LinkedHashMap -> Si necesitamos un HashMap y tambien nos importa el orde los elementos entonces LinkedHashMap es una buena opcion.
+     *                  es como un hashmap pero con sus elementos respaldados por una lista enlazada que garantiza que los iteradores retornen
+     *                  los elementos en el mismo orden en el que fueron creados.
+     */
+
     private static void treeMapTest() {
+        /**
+         * Implementacion de Map
+         * TreeMap -> es un diccionario que guarda los elementos de manera ordenada utilizando el comparador de cada objeto. Lo iteradores retornan
+         *            la informacion ordenada. Por ordenada nos referimos al orden natural de las llaves.
+         */
         Map map = new TreeMap();
         map.put("x", "equis");
         map.put("c", "asdffa");
@@ -186,6 +277,10 @@ public class HCollections {
         }
     }
 
+    /**
+     * Implementacion de Map
+     * Properties -> es un diccionario que representa un conjunto de datos persistentes, es decir que se puede guardar y cargar en memoria.
+     */
     private static void propertiesTest1() {
         Properties props = new Properties();
         props.put("color", "rojo");
@@ -194,16 +289,18 @@ public class HCollections {
 
         System.out.println(props.get("mensaje"));
 
-        try (OutputStream out = new FileOutputStream("c:/temp/params.props")) {
+        // guardamos en disco el archivo de propiedades
+        try (OutputStream out = new FileOutputStream("/home/ceva/Documents/params.props")) {
             props.store(out, "Propiedades de mi aplicacion.");
         } catch (IOException e) {
             System.out.println("Error de escritura.");
         }
     }
 
+    // lectura de un archivo de propiedades
     private static void propertiesTest2() {
         Properties props = new Properties();
-        try (InputStream in = new FileInputStream("c:/temp/params.props")) {
+        try (InputStream in = new FileInputStream("/home/ceva/Documents/params.props")) {
             props.load(in);
         } catch (IOException e) {
             System.out.println("Error de lectura.");
@@ -213,15 +310,15 @@ public class HCollections {
     }
 
     public static void main(String[] args) {
-        testSet();
-        equalsTest();
+        // testSet();
+        // equalsTest();
         // iteratorTest();
         // linkedHashSetTest();
         // treeSetTest();
         // listTest();
         // hashMapTest();
         // treeMapTest();
-        // propertiesTest1();
+        propertiesTest1();
         // propertiesTest2();
     }
 }
---
+++ src/com/ceva/section7/apijava2/JTextFormatterInNColumns.java
@@ -0,0 +1,148 @@
+package com.ceva.section7.apijava2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Dado un espacio limitado, pasamos a escribir un texto, una vez completada la columna, pasamos a la sgte columna y seguimos
+ * escribiendo el texto. En lugar de imprimir en pantalla cada palabra hasta completar la linea empleamos una coleccion y agregamos
+ * en ella las lineas de texto para al final imprimir el texto de la coleccion y formatear la salida.
+ */
+public class JTextFormatterInNColumns {
+    private static String getData()throws IOException{
+        StringBuilder str = new StringBuilder();
+
+        try(
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(JTextFormatterInNColumns.class.getResourceAsStream("resources/magna-carta.txt")))
+        ){
+            String line = br.readLine();
+            while(line != null){
+                str.append(line);
+                line = br.readLine();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) throws IOException {
+        String str1 = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo " +
+                "que vivía un hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y " +
+                "galgo corredor. Una olla de algo más vaca que carnero, salpicón las más noches, " +
+                "duelos y quebrantos los sábados, lantejas los viernes, algún palomino de añadidura " +
+                "los domingos, consumían las tres cuartas partes de su hacienda. El resto della " +
+                "concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos " +
+                "de lo mesmo, y los días de entresemana se honraba con su vellorí de lo más fino. " +
+                "Tenía en su casa una ama que pasaba de los cuarenta, y una sobrina que no llegaba " +
+                "a los veinte, y un mozo de campo y plaza, que así ensillaba el rocín como tomaba " +
+                "la podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de " +
+                "complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo de " +
+                "la caza. Quieren decir que tenía el sobrenombre de Quijada, o Quesada, que en " +
+                "esto hay alguna diferencia en los autores que deste caso escriben; aunque, por " +
+                "conjeturas verosímiles, se deja entender que se llamaba Quejana. Pero esto importa " +
+                "poco a nuestro cuento; basta que en la narración dél no se salga un punto de la " +
+                "verdad.";
+
+
+        int columns = 3; // # columnas a mostrar
+        int lineWidth = 35;
+
+// impresion de guia
+        for (int n=0; n<lineWidth; n++) {
+            // print 9 espacios y a la posicion 10 imprimimos numero
+            if (((n+1) % 10) == 0)
+                System.out.print(((n/10)+1) % 10);
+            else
+                System.out.print(" ");
+        }
+        System.out.println();
+        // imprimimos las unidades debajo de las decenas
+        for (int n=0; n<lineWidth; n++) {
+            System.out.print((n+1) % 10);
+        }
+        System.out.println();
+        // impresion de linea -
+        for (int n=0; n<lineWidth; n++)
+            System.out.print("-");
+        System.out.println();
+// fin impresion de guia
+
+        int curPos = 0;
+        List<String> lines = new ArrayList<>(); // coleccion de lineas de texto
+        StringBuilder curLine = new StringBuilder(); // linea de texto
+
+        boolean space = false;
+        StringTokenizer st = new StringTokenizer(getData()); // pasamos una cadena y devuelve los elementos de la cadena separado por espacios
+        // mientras haya palabras
+        while (st.hasMoreTokens()) {
+            // obtenemos la palabra
+            String s = st.nextToken();
+            // validacion: si la longitud de la palabra + lo acumulado es mayor al tamano de la columna
+            if ((curPos + s.length()) > lineWidth) {
+                // cuando se excede la longitud maxima de la linea, agregamos la linea a la coleccion de lineas de texto
+                lines.add(curLine.toString());
+                curLine.setLength(0); // limpiamos la linea para contener el texto de la sgte linea.
+                curPos = 0; // reset posicion del cursor
+                space = false;
+            }
+            // validacion: si antes de colocar una palabra debemos agregar un espacio o no.
+            if (space)
+                curLine.append(" "); // agregamos un espacio para colocar la sgte palabra en la linea actual.
+            curLine.append(s); // agregamos la palabra en la linea actual.
+            curPos += s.length() + 1; // actualizamos la posicion actual (el tamano de la linea que llevamos escrito)
+            space = true;
+        }
+        // validacion: si la longitud de la linea de texto es mayor a 0, agregamos lo ultimo que se acumulo a la coleccion.
+        if (curLine.length() > 0)
+            lines.add(curLine.toString());
+
+// formateo en columnas
+        /**
+         * formula para calcular las filas necesarias.
+         * Esta formula retorna la division de dos numeros redondeada al siguiente numero entero, lo redondeamos al sgte entero para
+         * aseguarnos que alcance el espacio.
+         * Ejemplo: # elementos 10 / 3 # columns -> el resultado es 3.33, si usamos int el resultado es truncado a 3 pero con la formula el resultado es redondeado a 4
+         * La formula seria: (10 + (3-1)) / 3
+         * 10 -> # filas
+         * 3 -> # columnas
+         */
+        // rows -> # de renglones que se imprimira al final
+        int rows = (lines.size() + (columns-1)) / columns;
+
+        /**
+         * Cada linea de la coleccion de texto estara compuesta por 3 elementos de la lista
+         * 1er renglon -> primer elemento de la lista + el elemento rows * 1 + el elemento rows * 2
+         * Los primeros rows renglones forman la primer columna de texto.
+         */
+// impresion rows en renglones
+        for (int n=0; n<rows; n++) {
+            curLine.setLength(0);
+            int col = 0; // # columna
+            // mientras el #columnas sea menor al # de columnas a imprimir
+            while (col < columns) {
+                // validamos que no se exceda
+                if ((rows*col + n) < lines.size()) {
+                    // 1 iteracion -> row*0+0
+                    // 2 iteracion -> row*1+0
+                    // 3 iteracion -> row*2+0
+                    curLine.append(lines.get(rows*col + n));
+                    if (col < (columns-1)) {
+                        while (curLine.length() < (lineWidth + 4)*(col+1))
+                            curLine.append(" ");
+                    }
+                }
+                col++; // incremento columna
+            }
+            System.out.println(curLine.toString());
+        }
+// fin impresion rows
+        System.out.println();
+    }
+}
